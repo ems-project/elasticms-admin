c64f61a14c086b951a5eeeb1db2e6b9b4e488f72
Mathieu De Keyzer <mathieu@dekeyzer.net>
2017-09-06 19:37:16 +0200

    Basic fonctional phpunit tests to have some a base for liveness script

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -11,7 +11,28 @@ class DefaultControllerTest extends WebTestCase
         $client = static::createClient();
 
         $crawler = $client->request('GET', '/');
-
+        $this->assertEquals(401, $client->getResponse()->getStatusCode());
+        $this->assertContains('Sign in to start your session', $crawler->filter('p.login-box-msg')->text());
+        
+        
+    }
+    
+    public function testLogin()
+    {
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/login');
+        
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Sign in to start your session', $crawler->filter('p.login-box-msg')->text());
+    }
+    
+    public function testHealthCheck()
+    {
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/health_check');
+        
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertContains('Welcome to Symfony', $crawler->filter('#container h1')->text());
     }
