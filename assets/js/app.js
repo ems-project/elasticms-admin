3ca083faea468e7c5b7e401cd7f2182e96bbec62
theus deka <me@theus.be>
2018-09-01 11:36:12 +0200

    Yarn hello world

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// app.js
+
+const $ = require('jquery');
+//window.$ = $;
+//window.jQuery = $;
+
+$(document).ready(function() {
+    console.log('Hello world, welcome to elasticms');
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,22 @@
 var Encore = require('@symfony/webpack-encore');
+var path = require('path');
 
 Encore
-    // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
+    .setManifestKeyPrefix('build/')
+
     .cleanupOutputBeforeBuild()
+    .autoProvidejQuery()
+    .enablePostCssLoader()
     .enableSourceMaps(!Encore.isProduction())
-    // uncomment to create hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
+    .enableSassLoader(function(sassOptions) {}, {
+        resolveUrlLoader: false
+    })
 
-    // uncomment to define the assets of the project
-    // .addEntry('js/app', './assets/js/app.js')
-    // .addStyleEntry('css/app', './assets/css/app.scss')
-
-    // uncomment if you use Sass/SCSS files
-    // .enableSassLoader()
-
-    // uncomment for legacy applications that require $/jQuery as a global variable
-    // .autoProvidejQuery()
+    .addStyleEntry('css/app', './assets/css/app.scss')
+    .addEntry('js/app', './assets/js/app.js')
 ;
 
-module.exports = Encore.getWebpackConfig();
+var config = Encore.getWebpackConfig();
+module.exports = config;
