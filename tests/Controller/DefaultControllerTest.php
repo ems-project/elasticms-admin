ed5e26acb23ddb92f146f6951a76e5e34004a533
lame <laurens.mertens@smals.be>
2019-12-03 09:05:47 +0100

    Latest version - no private or client related data

---
+++ tests/Controller/DefaultControllerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Tests\AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class DefaultControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        $this->assertEquals(401, $client->getResponse()->getStatusCode());
+        $this->assertContains('Sign in to start your session', $crawler->filter('p.login-box-msg')->text());
+        
+        
+    }
+    
+    public function testLogin()
+    {
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/login');
+        
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Sign in to start your session', $crawler->filter('p.login-box-msg')->text());
+    }
+    
+    public function testHealthCheck()
+    {
+        $client = static::createClient();
+        
+        $crawler = $client->request('GET', '/health_check');
+        
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertContains('Status of the cluster', $crawler->filter('h1')->text());
+    }
+}
