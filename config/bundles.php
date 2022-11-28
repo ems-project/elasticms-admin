0f26336c059ff84d8f0c70e1106c853ef939f8ec
David mattei <d.mattei89@gmail.com>
2022-11-29 00:18:18 +0100

    build: 4.3.0

--- config/bundles.php
+++ config/bundles.php
@@ -1,34 +1,19 @@
 <?php
 
-use Symfony\Bundle\FrameworkBundle\FrameworkBundle;
-use Symfony\Bundle\TwigBundle\TwigBundle;
-use Symfony\Bundle\SecurityBundle\SecurityBundle;
-use Doctrine\Bundle\DoctrineBundle\DoctrineBundle;
-use Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle;
-use Symfony\Bundle\MonologBundle\MonologBundle;
-use Symfony\Bundle\WebProfilerBundle\WebProfilerBundle;
-use Symfony\Bundle\DebugBundle\DebugBundle;
-use EMS\CoreBundle\EMSCoreBundle;
-use EMS\CommonBundle\EMSCommonBundle;
-use EMS\ClientHelperBundle\EMSClientHelperBundle;
-use EMS\FormBundle\EMSFormBundle;
-use EMS\SubmissionBundle\EMSSubmissionBundle;
-use Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle;
-use Twig\Extra\TwigExtraBundle\TwigExtraBundle;
 return [
-    FrameworkBundle::class => ['all' => true],
-    TwigBundle::class => ['all' => true],
-    SecurityBundle::class => ['all' => true],
-    DoctrineBundle::class => ['all' => true],
-    DoctrineMigrationsBundle::class => ['all' => true],
-    MonologBundle::class => ['all' => true],
-    WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    DebugBundle::class => ['dev' => true, 'test' => true],
-    EMSCoreBundle::class => ['all' => true],
-    EMSCommonBundle::class => ['all' => true],
-    EMSClientHelperBundle::class => ['all' => true],
-    EMSFormBundle::class => ['all' => true],
-    EMSSubmissionBundle::class => ['all' => true],
-    SensioFrameworkExtraBundle::class => ['all' => true],
-    TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
+    EMS\CoreBundle\EMSCoreBundle::class => ['all' => true],
+    EMS\CommonBundle\EMSCommonBundle::class => ['all' => true],
+    EMS\ClientHelperBundle\EMSClientHelperBundle::class => ['all' => true],
+    EMS\FormBundle\EMSFormBundle::class => ['all' => true],
+    EMS\SubmissionBundle\EMSSubmissionBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
