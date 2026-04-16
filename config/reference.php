65eae720d1c6fd17e2761c13dc2430236e337859
David mattei <d.mattei89@gmail.com>
2026-04-16 09:09:20 +0200

    chore: rector, phpstan, phpcs, linting and format docs (#1659)

--- config/reference.php
+++ config/reference.php
@@ -1696,7 +1696,7 @@ use Symfony\Component\Config\Loader\ParamConfigurator as Param;
  *             use_underscore?: bool|Param, // Default: true
  *             unordered_list_markers?: list<scalar|Param|null>,
  *         },
- *         ...<mixed>
+ *         ...<string, mixed>
  *     },
  * }
  * @psalm-type ConfigType = array{
